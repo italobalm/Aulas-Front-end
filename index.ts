a0c3385f119d1ecdedbb9add60b2cae0c080eb2e
Italo Breno <166156315+italobalm@users.noreply.github.com>
2024-08-05 23:47:23 -0300

    At 5

--- index.ts
+++ index.ts
@@ -1,262 +1,200 @@
 //Questão 1
-let numero = (Number(prompt("Digite um número inteiro:")));
 
-if (numero > 0){
-    console.log("Positivo!");
-} else if (numero < 0){
-    console.log("Negativo!");
-} else {
-    console.log("Número igual a 0")
-};
+//O comando "for" é utilizado como loop para repetir algo enquanto uma condição 
+//for verdadeira e sabe-se com antecedência quantas vezes deseja repetir uma ação.
 
 //Questão 2
-let idade = (Number(prompt("Digite a sua idade:")));
 
-if (idade >= 18){
-    alert("Maior de idade")
-} else {
-    alert("Menor de idade")
-};
+//O incio é definido através da declaração de uma variável que será controlada 
+//ao longo do código. Ex: for (let i = 0;). A repetição inicia no 0.
 
 //Questão 3
-let numero1 = (Number(prompt("Digite o primeiro número inteiro:")));
-let numero2 = (Number(prompt("Digite o segundo número inteiro:")));
-
-if (numero1 > numero2){
-    alert(`Número ${numero1} é o maior`)
-} else if (numero2 > numero1){
-    alert(`Número ${numero2} é o maior`)
-} else {
-    alert("Os números são iguais")
-};
+//O fim da execução é definido logo após a declaração do ponto de partida,
+//onde se diz até que momento aquela repetição acontecerá.
+//Ex: for (let i = 0; i < 10). A repetição inicia em 0 e continua enquanto i for menor que 10.
 
 //Questão 4
-let num = (Number(prompt("Digite um número:")));
 
-if (num % 2 == 0){
-    alert("O número é par")
-} else {
-    alert("O número é ímpar")
-};
+//A alteração do valor ocorre através do incremento "++".
+//Ex: for (let i = 0; i < 10; i++){}.
 
 //Questão 5
-let nota1 = (Number(prompt("Insira a primeira nota:")));
-let nota2 = (Number(prompt("Insira a segunda nota:")));
-let nota3 = (Number(prompt("Insira a terceira nota:")));
-let media = (nota1 + nota2 + nota3)/3
-
-if (media >= 7){
-    alert("Aprovado!")
-} else {
-    alert("Reprovado!")
+for (let i = 0; i < 10; i++){
+    console.log("Testando uma frase!");
 };
 
 //Questão 6
-let nome1 = (String(prompt("Digite o nome da primeira pessoa:")));
-let nome2 = (String(prompt("Digite o nome da segunda pessoa:")));
-
-if (nome1.length > nome2.length){
-    alert(`O nome ${nome1} possui mais caracteres!`)
-} else {
-    alert(`O nome ${nome2} possui mais caracteres!`)
+for (let y = 0; y < 10; y++){
+    console.log(y);
 };
 
 //Questão 7
-let caractere = (String(prompt("Digite um caractere:"))).toUpperCase();
+for (let z = 0; z < 10; z++){
+    
+    if (z == 5){
+        break;
+    };
 
-if (caractere == "A" || caractere == "E" || caractere == "I" || caractere == "O" || caractere == "U"){
-    alert("É uma vogal!");
-} else {
-    alert("É uma consoante!");
+    console.log(z);
 };
 
 //Questão 8
-let n1 = (Number(prompt(" Digite o primeiro número:")));
-let n2 = (Number(prompt(" Digite o segundo número:")));
-let n3 = (Number(prompt(" Digite o terceiro número:")));
+for (let x = 0; x < 10; x++){
 
-if (n1 > n2 && n1 < n3){
-    alert(`${n3}, ${n1}, ${n2}`);
-} if (n1 > n3 && n1 < n2){
-    alert(`${n2}, ${n1}, ${n3}`);
-} if (n3 > n1 && n3 < n2){
-    alert(`${n2}, ${n3}, ${n1}`);
-} if (n3 > n2 && n3 < n1){
-    alert(`${n1}, ${n3}, ${n2}`);
-} if (n2 > n1 && n2 < n3){
-    alert(`${n3}, ${n2}, ${n1}`);
-} if (n2 > n3 && n2 < n1){
-    alert(`${n1}, ${n2}, ${n3}`);
+    if (x == 5){
+        continue;
+    };
+
+    console.log(x);
 };
 
 //Questão 9
-let peso = (Number(prompt("Digite seu peso:")));
-let altura = (Number(prompt("Digite a sua altura:")));
-let imc = altura**altura/peso
+let nomes = ['João', 'Paulo', 'Pedro', 'Gustavo', 'Maria'];
 
-if (imc < 18.5){
-    alert("Abaixo do peso!");
-} else if (imc >= 18.5 && imc <= 24.9){
-    alert("Peso normal!");
-} else if (imc >= 30 && imc <= 34.9) {
-    alert("Sobrepeso!");
-} else if (imc >= 35 && imc <= 39.9){
-    alert("Obeso!");
-} else {
-    alert("Muito obeso!");
+for (let n of nomes){
+    console.log(n);
 };
 
 //Questão 10
-let mes = (Number(prompt("Digite o número do mÊs:")));
-
-if (mes == 1){
-    alert("Janeiro");
-}
-if (mes == 2){
-    alert("Fevereiro");
-}
-if (mes == 3){
-    alert("Março");
-}
-if (mes == 4){
-    alert("Abril");
-}
-if (mes == 5){
-    alert("Maio");
-}
-if (mes == 6){
-    alert("Junho");
-}
-if (mes == 7){
-    alert("Julho");
-}
-if (mes == 8){
-    alert("Agosto");
-}
-if (mes == 9){
-    alert("Setembro");
-}
-if (mes == 10){
-    alert("Outubro");
-}
-if (mes == 11){
-    alert("Novembro");
-}
-if (mes == 12){
-    alert("Dezembro");
-} else {
-    alert("Número não corresponde a um mêS");
+for (let h = 1; h < 11; h++){
+    console.log(h);
 };
 
 //Questão 11
-let salario = (Number(prompt("Digite o valor de seu salário:")));
-let aumento = 0;
-
-if (salario > 1500){
-    aumento = salario*0.10
-    alert(`Seu aumento será de R$ ${aumento}, Novo salário: R$ ${salario + aumento}`);
-} else {
-    aumento = salario*0.15
-    alert(`Seu aumento será de R$ ${aumento}, Novo salário: R$ ${salario + aumento}`);
+for (let g = 10; g > 0; g--){
+    console.log(g);
 };
 
 //Questão 12
-let numeroInteiro = (Number(prompt("Digite um número inteiro:")));
+let soma = 0;
 
-if (numeroInteiro % 3 == 0 && numeroInteiro % 5 == 0){
-    alert("Divisível por 3 e 5");
-} else {
-    alert("O número não é divisível por 3 e 5");
+for (let num = 0; num < 101; num++){
+    soma = soma + num;
 };
+console.log(soma);
 
 //Questão 13
-let diaSemana = (String(prompt("Digite o dia da semana:"))).toUpperCase();
+for (let p = 1; p < 51; p++){
 
-if (diaSemana == "SEGUNDA" || "TERÇA" || "QUARTA" || "QUINNTA" || "SEXTA"){
-    alert("Dia útil");
-} else (
-    alert("Fim de semana")
-);
+    if (p % 2 == 0){
+        console.log(p);
+    };
+};
 
 //Questão 14
-let n = (Number(prompt("Digite um número de 1 a 5:")));
+let produto = 1;
 
-switch (n) {
-    case 5:
-        alert("Muito bom")
-        break;
-    case 4:
-        alert("Bom")
-        break;
-    case 3:
-        alert("Regular")
-        break;
-    case 2:
-        alert("Insuficiente")
-        break;
-    case 1:
-        alert("Muito insuficiente")
-        break;
-    default:
-        break;
+for (let u = 1; u < 6; u++){
+    produto = produto * u;
 };
+console.log(produto);
 
 //Questão 15
-let dSemana = (Number(prompt("Digite um número de 1 a 7 para verificar o dia da semana:")));
-
-if (dSemana == 1){
-    alert("Domingo");
-}
-if (dSemana == 2){
-    alert("Segunda");
-}
-if (dSemana == 3){
-    alert("Terça");
-}
-if (dSemana == 4){
-    alert("Quarta");
-}
-if (dSemana == 5){
-    alert("Quinta");
-}
-if (dSemana == 6){
-    alert("Sexta");
-}
-if (dSemana == 7){
-    alert("Sábado");
+for (let b = 1; b < 11; b++){
+    console.log(`7x${b} = ${7*b}`);
 };
 
-//Questão 17
-let idadeUsuario = (Number(prompt("Digite a sua idade:")));
+//Questão 16
+//let somar = 0;
 
-if (idadeUsuario >=0 && idadeUsuario <=1){
-    alert("Bebê");
-}
-if (idadeUsuario > 1 && idadeUsuario <= 12){
-    alert("Criança");
-}
-if (idadeUsuario >= 13 && idadeUsuario <= 18){
-    alert("Adolescente");
-} else {
-    alert("Adulto");
+//for (let w = 0; w < 5; w++){
+//    let notas = (Number(prompt(`Digite a ${w}º nota:`)));
+ //   somar = somar + notas;
+//};
+//console.log(`A média das notas é igual a ${somar/5}`);
+
+//Questão 17
+for (let d = 1; d < 51; d++){
+
+    if (d % 3 == 0){
+        console.log(d);
+    };
 };
 
 //Questão 18
-let estadoCivil = (String(prompt("Digite seu estado civil:"))).toUpperCase();
+let maiorNumero = 0;
+let menorNumero = 554874873487548;
+for (let t = 1; t < 11; t++){
+    let numero = (Number(prompt(`Digite o ${t}º número:`)));
 
-switch (estadoCivil) {
-    case "SOLTEIRO":
-        alert("Você é solteiro!")
-        break;
-    case "CASADO":
-        alert("Você é casado!")
-        break
-    case "VIÚVO":
-        alert("Você é viúvo!")
-        break
-    case "DIVORCIADO":
-        alert("Você é divorciado!")
-        break
-    default:
-        break;
+    if (numero > maiorNumero){
+        maiorNumero = numero;
+    };
+    
+    if (numero  < menorNumero){
+        menorNumero = numero;
+    };
 };
+console.log(`Maior número: ${maiorNumero} Menor número: ${menorNumero}`);
+
+//Questão 19
+for (let q = 1; q < 101; q++){
+
+    if (q % 2 != 0){
+        console.log(q);
+    };
+};
+
+//Questão 20
+let aprovados = 0;
+
+for (let f = 1; f < 6; f++){
+    let nota = (Number(prompt(`Digite a nota do ${f}º aluno:`)));
+
+    if (nota >= 7){
+        aprovados +=1;
+    }
+};
+console.log(`${aprovados} alunos foram aprovados!`);
+
+//Questão 21
+let numInteiro = (String(prompt("Digite um número inteiro:")));
+let adicao = 0;
+
+for (let s = 0; s < numInteiro.length; s++){
+    adicao += parseInt(numInteiro[s]);
+};
+
+//Questão 22
+for (let k = 1; k < 101; k++){
+    let inteiro = (Number(prompt("Digite um número inteiro:")));
+
+    if (inteiro % k == 0){
+        console.log(k);
+    };
+};
+
+//Questão 23
+let somarAlturas = 0;
+for (let a = 1; a < 6; a++){
+    let altura = (Number(prompt(`Digite a ${a}º altura:`)));
+    somarAlturas += altura;
+};
+console.log(`A média de alturas é ${somarAlturas/5}`);
+
+//Questão 24
+for (let c = 1; c < 101; c++){
+    
+    if (c % 3 == 0 && c % 5 != 0){
+        console.log("Fizz");
+    } else if (c % 5 == 0 && c % 3 != 0) {
+        console.log("Buzz");
+    } else if (c % 3 == 0 && c % 5 == 0){
+        console.log("FizzBuzz");
+    } else {
+        console.log(c);
+    };
+};
+
+//Questão 25
+let somaPares = 0;
+let numeral = (String(prompt("Digite um número inteiro:")));
+
+
+//Questão 26
+let valorInvertido: string = '';
+let digito = (String(prompt("Digite um número inteiro:")));
+for (let u = digito.length - 1; u >= 0; u--){
+    valorInvertido += digito[u];
+};
+console.log(valorInvertido);
